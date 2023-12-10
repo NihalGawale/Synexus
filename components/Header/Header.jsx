a9ba1b9cd435c420d74b1d7a15ac6cd4e61a6a4a
Nihal Gawale <nihalgawlae123@gmail.com>
2023-12-10 13:46:46 +0530

    working on header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Header = () => {
   // const router
   return (
-    <div className="  bg-[#f3f3f3] w-[100%] lg:w-[70%] p-3 lg:p-4 flex justify-center items-center text-3xl rounded-full font-semibold font border-b shadow-lg shadow-gray-500/30 cursor-pointer">
+    <div className="  bg-[#f3f3f3] w-[100%] lg:w-[70%] p-3 lg:p-4 flex justify-center items-center text-[21px] lg:text-3xl rounded-full font-semibold font border-b shadow-lg shadow-gray-500/30 cursor-pointer">
       <div className="flex justify-between items-center w-[90%]">
         <p className="">Synexus</p>
         <div className="text-sm lg:text-base text-[#7d7b7b] flex font-medium space-x-4 lg:space-x-10">
