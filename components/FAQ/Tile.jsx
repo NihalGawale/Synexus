4f2c67e117a00e66fd55b16efe3bfd9291843964
Nihal Gawale <nihalgawlae123@gmail.com>
2023-12-17 00:10:04 +0530

    code refactor

--- components/FAQ/Tile.jsx
+++ components/FAQ/Tile.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Tile = () => {
+const Tile = ({question,answer}) => {
   return (
-    <div class="max-w-xs w-[300px] bg-white text-center border border-gray-300 rounded-lg shadow-xl shadow-gray-500/40 flex flex-col  p-5 space-y-5">
-        <h1 className='text-xl font-semibold'>What is the purpose of this app?</h1>
-        <p className='font-semibold lg:text-[16px] text-[#616060] '>The problem we aim to address is the gap between freelancers or individuals who need assistance with specific tasks and skilled professionals who are willing to offer their services during their free time. </p>
+    <div class="max-w-xs w-[300px] h-[350px] bg-white text-center border border-gray-300 rounded-lg shadow-md shadow-gray-500/40 flex flex-col  p-5 space-y-5">
+        <h1 className='text-xl font-semibold'>{question}</h1>
+        <p className='font-medium lg:text-[16px] text-[#616060] '>{answer}</p>
     </div>
   );
 }
